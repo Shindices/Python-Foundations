677ad509ae6cc42dae256fbe4224038480b41738
Oluwalewa Oyeniyi <lewaedward3@gmail.com>
2025-12-26 03:11:06 -0800

    grade evaluator using if, elif & else

---
+++ grade_evaluator.py
@@ -0,0 +1,10 @@
+Rules:90+ → A 80–89 → B 70–79 → C <70 → F
+score = float(input("Enter score: "))
+if score >= 90:
+  print("A")
+elif score >= 80:
+  print("B")
+elif score >= 70:
+  print("C")
+else:
+  print("F")
